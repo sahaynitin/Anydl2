24ee30bb0bef4d2b3598a6d747feb7c31c4e03f2
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-05 13:01:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,20 +25,6 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(
-        root="plugins"
-    )
-    app = pyrogram.Client(
-        "AnyDLBot",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins,
-        workers=10
-    )
+        root="plugins"    
     anydlx = dict(
-    root="anydlx"
-    )
-    app = "AnyDLBot",
-        anydlx=anydlx
-    )
-    app.run()
+        root="anydlx"
