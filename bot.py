09d232836d78e04ad05d3bc12ad1ce9831d633de
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-05 12:35:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,8 @@ if __name__ == "__main__" :
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins,Any-Dl-X,
+        plugins=plugins,
+        Any-Dl-X=Any-Dl-X,
         workers=10
     )
     app.run()
