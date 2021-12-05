425770a923ede5eb572239a03f148508532141ae
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-05 12:55:09 +0530

    Update dl_button.py

--- anydlx/dl_button.py
+++ anydlx/dl_button.py
@@ -22,7 +22,7 @@ else:
     from config import Config
 # the Strings used for this "thing"
 from translation import Translation
-from Any-Dl-X.thumbnail import *
+from anydlx.thumbnail import *
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
 from hachoir.metadata import extractMetadata
